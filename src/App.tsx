3ac3ec3b0140be6a9f17ba5a1b0e248036221faf
Abdullah Al Faiyaz <abdullah-al.faiyaz@stud.uni-bamberg.de>
2025-03-05 05:31:12 +0100

    ExpenseFilter component done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 // import Expanse from "./components/Expanse";
 import ExpanseList from "./expanse-tracker/components/ExpanseList";
+import ExpenseFilter from "./expanse-tracker/components/ExpenseFilter";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
   const [expenses, setExpenses] = useState([
     { id: 1, description: "aaa", amount: 5, category: "Utilities" },
     { id: 2, description: "bbb", amount: 50, category: "Utilities" },
@@ -10,11 +13,20 @@ function App() {
     { id: 4, description: "ddd", amount: 5, category: "Utilities" },
   ]);
 
+  const visibleExpress = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
   return (
     <>
       {/* <Expanse></Expanse> */}
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        ></ExpenseFilter>
+      </div>
       <ExpanseList
-        expenses={expenses}
+        expenses={visibleExpress}
         onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       ></ExpanseList>
     </>
